796e295ac2f56ac7bab74685ae5d6fc891545f8f
soohyun <soohyun_kim2@tmax.co.kr>
2022-10-14 16:48:44 +0900

    change profile

--- pages/introduce/member.tsx
+++ pages/introduce/member.tsx
@@ -4,7 +4,7 @@ import Image from 'next/image';
 import member1 from '../../assets/member1.jpg';
 import member3 from '../../assets/member3.jpg';
 import member4 from '../../assets/member4.jpg';
-import member2 from '../../assets/member2.jpg';
+import member2 from '../../assets/member2.png';
 
 const Member: NextPage = () => {
   return (

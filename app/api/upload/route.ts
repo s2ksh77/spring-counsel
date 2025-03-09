069f77db8421b01a7b91e21a73daa4f0345e5867
soohyun <soohyun_kim2@tmax.co.kr>
2025-03-09 22:29:35 +0900

    fix(app-router): upload 서비스 수정

---
+++ app/api/upload/route.ts
@@ -0,0 +1,37 @@
+import { NextRequest, NextResponse } from 'next/server';
+import client from '@libs/server/client';
+import { getUser } from '@libs/server/session';
+
+const svcObjecct = {
+  reservation: 'reservationFile',
+  notice: 'noticeFile',
+  review: 'reviewFile',
+};
+
+export async function POST(req: NextRequest) {
+  try {
+    const user = await getUser(req);
+
+    if (!user) {
+      return NextResponse.json({ ok: false, error: 'Unauthorized' }, { status: 401 });
+    }
+
+    const { name, url, form, id, fileId } = await req.json();
+
+    await client[svcObjecct[form]].create({
+      data: {
+        id: fileId,
+        name,
+        url,
+        [form]: {
+          connect: { id },
+        },
+      },
+    });
+
+    return NextResponse.json({ ok: true });
+  } catch (error) {
+    console.error('Upload Error:', error);
+    return NextResponse.json({ ok: false, error: 'Failed to upload file' }, { status: 500 });
+  }
+}
